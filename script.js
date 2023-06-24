45b16171bb9408f21d39f49e3bd279b8a8141bab
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-06-24 07:01:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-//your code here
+let div = document.getElementById('app');
+let button = document.getElementById('swap');
+
+button.addEventListener('click', function() {
+  if (div.classList.contains('day')) {
+    div.classList.remove('day');
+    div.classList.add('night');
+    button.classList.remove('button_day');
+    button.classList.add('button_night');
+  } else {
+    div.classList.remove('night');
+    div.classList.add('day');
+    button.classList.remove('button_night');
+    button.classList.add('button_day');
+  }
+});
 
